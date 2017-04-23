227dd111748c608badfe571ec02968d789d45010
Dustin Horvath <dustinmhorvath@gmail.com>
2017-04-23 15:47:53 -0500

    Hardcoded to older ruby version.

--- install.sh
+++ install.sh
@@ -29,10 +29,11 @@ DATE=$(date +"%Y%m%d%H%M")
 #make > /dev/null
 #make install-conf > /dev/null
 
-apt-get install curl > /dev/null
+sudo apt-get install curl -y > /dev/null
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -L https://get.rvm.io | bash -s stable --ruby
+gpg --keyserver hkp://pgp.mit.edu --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -L https://get.rvm.io | bash
+rvm install 2.2.3
 source /usr/local/rvm/scripts/rvm
 echo "source /usr/local/rvm/scripts/rvm" >> /root/.bashrc
 
@@ -44,7 +45,7 @@ debconf-set-selections <<< "mysql-server mysql-server/root_password_again passwo
 sudo apt-get -y install mysql-server > /dev/null
 echo "Snorby dependencies (3/3) gem dependencies..."
 
-gem install thor i18n bundler tzinfo builder memcache-client rack rack-test erubis mail rack-mount rails sqlite3 > /dev/null
+gem install thor i18n bundler tzinfo builder memcache-client rack rack-test erubis mail rack-mount rails sqlite3
 echo "Done."
 echo ""
 
